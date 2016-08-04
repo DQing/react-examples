5579285b76300d300e34f2045b7d318f50b14d8c
douqing <15829258737@163.com>
2016-08-04 11:42:26 +0800

    学习state和props的区别以及数据传递

--- js/app.js
+++ js/app.js
@@ -1,1 +1,43 @@
-// TODO
+const App = React.createClass({
+    getInitialState(){
+      return{
+          name:"douqing",
+          password:"111111"
+      }  
+    },
+    onEditor:function(value){
+        this.setState({password:value});
+    },
+    render: function () {
+        return (
+            <div>
+                <User name = {this.state.name}/>
+                <Password password = {this.state.password} onEditor = {this.onEditor}/>
+            </div>
+        )
+    }
+});
+const User = React.createClass({
+    render: function () {
+        return (
+            <div>
+                <p>{this.props.name}</p>
+            </div>
+        )
+    }
+});
+const Password = React.createClass({
+    editor:function () {
+        this.props.onEditor("123456");
+    },
+    render: function () {
+        return (
+            <div>
+                <p>{this.props.password}</p>
+                <button onClick = {this.editor}>editor</button>
+            </div>
+        )
+    }
+});
+ReactDOM.render(<App />, document.getElementById('content'));
+
